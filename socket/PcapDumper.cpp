454f5c77c344cd8c198cdf3c8bca4e89ba8ba553
mboretto <marco.bore@gmail.com>
2015-10-21 15:15:04 +0000

    Added as Option

--- socket/PcapDumper.cpp
+++ socket/PcapDumper.cpp
@@ -3,22 +3,21 @@
 
 namespace na62 {
 
-    const int PcapDumper::n_file_;
-    pcap_t* PcapDumper::handle_[n_file_];
-    pcap_dumper_t* PcapDumper::dumper_[n_file_];
+const int PcapDumper::n_file_;
+pcap_t* PcapDumper::handle_[n_file_];
+pcap_dumper_t* PcapDumper::dumper_[n_file_];
 
+void PcapDumper::dumpPacket(int file_n, char* packet, uint packet_leght) {
 
-    void PcapDumper::dumpPacket(int file_n, char* packet, uint packet_leght) {
+	pcap_pkthdr pcap_hdr;
+	pcap_hdr.caplen = pcap_hdr.len = packet_leght;
+	gettimeofday(&pcap_hdr.ts, NULL);
 
-      pcap_pkthdr pcap_hdr;
-      pcap_hdr.caplen = pcap_hdr.len = packet_leght;
-      gettimeofday(&pcap_hdr.ts, NULL);
+	pcap_dump((u_char *) dumper_[file_n], &pcap_hdr, (u_char *) packet);
+}
 
-      pcap_dump((u_char *)dumper_[file_n], &pcap_hdr, (u_char *) packet);
-    }
-
-    PcapDumper::PcapDumper() {
-    }
-    PcapDumper::~PcapDumper() {
-    }
+PcapDumper::PcapDumper() {
+}
+PcapDumper::~PcapDumper() {
+}
 }
